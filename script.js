96a162283c282e79e1818e1fe172af9c45f82291
skoba3678 <skobayankee@yahoo.com>
2023-12-10 01:45:34 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var num = prompt("Please Enter A Number", "");
+for(var i=1;i<=100;i++){
+    document.write("<div>");
+    document.write(num + " x " + i + " = " + i*num );
+    document.write("</div> <br>");
+}
